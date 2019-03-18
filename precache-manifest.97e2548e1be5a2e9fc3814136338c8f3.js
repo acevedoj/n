3f97b3c316d6cfecb639a8d1206428d937540d6e
acevedoj <acevedoj@uw.edu>
2019-03-17 21:46:58 -0700

    Updates

---
+++ precache-manifest.97e2548e1be5a2e9fc3814136338c8f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8a7edd6e0f73c9005511",
+    "url": "/n/static/css/main.5faa2d48.chunk.css"
+  },
+  {
+    "revision": "8a7edd6e0f73c9005511",
+    "url": "/n/static/js/main.e6604581.chunk.js"
+  },
+  {
+    "revision": "b8bdd4bd47bece53c3e6",
+    "url": "/n/static/js/runtime~main.31ffa4c8.js"
+  },
+  {
+    "revision": "e9d88d16b7f902e44ea6",
+    "url": "/n/static/js/2.c0eda0b7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/n/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "25d2e47284864825b7d69a09ab81f750",
+    "url": "/n/index.html"
+  }
+];
